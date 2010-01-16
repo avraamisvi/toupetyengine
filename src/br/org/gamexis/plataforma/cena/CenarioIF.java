87c595ad912628c0cf0155153f4e8053639ef119
abraao.isvi <abraao.isvi@ebfd35fc-a447-11dd-b47b-0378ca0a891c>
2010-01-16 21:16:19 +0000

    Atualizacao do codigo para a versao mais atual

--- src/br/org/gamexis/plataforma/cena/CenarioIF.java
+++
@@ -1,8 +0,0 @@
-package br.org.gamexis.plataforma.cena;
-
-public interface CenarioIF extends Cena {
-	
-	public void adicionarAnimacao(Animado anim, int nivel);
-	public void adicionarAtor(Ator ator, int nivel, int excluidos);
-	public void adicionarAtor(Ator ator, int nivel, int excluidos, boolean semCorpo);
-}
--- src/br/org/gamexis/plataforma/motor/Constantes.java
+++
@@ -1,5 +0,0 @@
-package br.org.gamexis.plataforma.motor;
-
-public interface Constantes {	
-	
-}
